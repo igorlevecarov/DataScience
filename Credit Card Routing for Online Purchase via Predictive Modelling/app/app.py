39f1372a7e67a3e4490089c73a10cb8e60fd4a74
Igor Levecarov <igor.levecarov@gmail.com>
2022-11-09 08:28:10 +0100

    create web app for prediction

--- Credit Card Routing for Online Purchase via Predictive Modelling/app/app.py
+++ Credit Card Routing for Online Purchase via Predictive Modelling/app/app.py
@@ -1,13 +1,12 @@
 from flask import Flask,request, url_for, redirect, render_template, jsonify
-from pycaret.regression import *
 import pandas as pd
 import pickle
 import numpy as np
 
 app = Flask(__name__)
 
-model = load_model('deployment_28042020')
-cols = ['age', 'sex', 'bmi', 'children', 'smoker', 'region']
+model = pickle.load(open('model.pkl', 'rb'))
+cols = ['country','amount', '3D_secured', 'card', 'weekday', 'hour']
 
 @app.route('/')
 def home():
@@ -17,17 +16,14 @@ def home():
 def predict():
     int_features = [x for x in request.form.values()]
     final = np.array(int_features)
-    data_unseen = pd.DataFrame([final], columns = cols)
-    prediction = predict_model(model, data=data_unseen, round = 0)
-    prediction = int(prediction.Label[0])
-    return render_template('home.html',pred='Expected Bill will be {}'.format(prediction))
+    input_data = pd.DataFrame([final], columns = cols)    
+    return render_template('home.html',pred='Best possible PSP is: {}'.format(model.predict(input_data)))
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     data = request.get_json(force=True)
-    data_unseen = pd.DataFrame([data])
-    prediction = predict_model(model, data=data_unseen)
-    output = prediction.Label[0]
+    input_data = pd.DataFrame([data])
+    output = model.predict(input_data)
     return jsonify(output)
 
 if __name__ == '__main__':
